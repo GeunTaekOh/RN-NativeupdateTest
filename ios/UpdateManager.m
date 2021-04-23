55e2f0e946efa2c4903ab65ac06d50032d4f69c5
GeunTaekOh <rmsxor94@naver.com>
2021-04-23 17:23:57 +0900

    [앱 아이콘 추가 + 설치url -> 업데이트url 로 변경]
    추후에 alert 를 네이티브나 rn 에서 추가해야함

--- ios/UpdateManager.m
+++ ios/UpdateManager.m
@@ -34,9 +34,12 @@
     
     NSString * urlString = [[NSString alloc] init];
     //        urlString = [NSString stringWithFormat:@"%@%@%@",@"https://autowayapps.hyundai.net/appstore/app/downloadApp.bin?osCode=iOS&file=updatetest_",serverVersion,@".ipa"];
-//    urlString = [NSString stringWithFormat:@"%@",@"https://autowayapps.hyundai.net/appstore/app/downloadApp.bin?osCode=iOS&file=hmg_test_app.ipa"];
+    //    urlString = [NSString stringWithFormat:@"%@",@"https://autowayapps.hyundai.net/appstore/app/downloadApp.bin?osCode=iOS&file=hmg_test_app.ipa"];
     
-    urlString = [NSString stringWithFormat:@"%@",@"itms-services://?action=download-manifest&url=https%3A%2F%2Fautowaymobile.hmc.co.kr%2Fappstore.app%2Ffile%2Fdownload.bin%3Ffilename%3Dhmc_test_app.plist%26osCode%3DiOS"];
+    //    urlString = [NSString stringWithFormat:@"%@",@"itms-services://?action=download-manifest&url=https%3A%2F%2Fautowaymobile.hmc.co.kr%2Fappstore.app%2Ffile%2Fdownload.bin%3Ffilename%3Dhmc_test_app.plist%26osCode%3DiOS"];
+    
+    urlString = [NSString stringWithFormat:@"%@",@"itms-services://?action=download-manifest&url=https://autowaymobile.hmc.co.kr:443/autoway.app/ios/update/hmc_test_app.plist"];
+    
     
     NSLog(@"url string : %@",urlString);
     
@@ -55,6 +58,43 @@
         //몇초동안 반응없으면 앱설치 안된다고 감지해야함
       }
     });
+    
+    ///
+//    NSString *updateTitle = [NSString stringWithFormat:@"%@",@"업데이트"];
+//    NSString *updateMessage = [NSString stringWithFormat:@"%@",@"새로운 버전의 APP이 존재합니다."];
+//    NSString *okButton = [NSString stringWithFormat:@"%@",@"확인"];
+//
+//    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:updateTitle
+//                                                                             message:updateMessage preferredStyle:UIAlertControllerStyleAlert];
+//    [alertController addAction:[UIAlertAction actionWithTitle:okButton style:UIAlertActionStyleCancel handler:^(UIAlertAction *action) {
+//      dispatch_async(dispatch_get_main_queue(), ^{
+//        if(IsAtLeastiOSVersion(@"10.0")){
+//          [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString] options:@{} completionHandler:^(BOOL success) {
+//            if(!success){
+//              //불가능한 메시지 출력
+//              NSLog(@"fail!!!!!!!");
+//            }else{
+//              NSLog(@"success!!!!!");
+//            }
+//          }];
+//        }else{
+//          [[UIApplication sharedApplication] openURL:[NSURL URLWithString:urlString]];
+//          //몇초동안 반응없으면 앱설치 안된다고 감지해야함
+//        }
+//      });
+//    }]];
+//
+//    id rootViewController = [UIApplication sharedApplication].delegate.window.rootViewController;
+//    if([rootViewController isKindOfClass:[UINavigationController class]])
+//    {
+//        rootViewController = ((UINavigationController *)rootViewController).viewControllers.firstObject;
+//    }
+//    if([rootViewController isKindOfClass:[UITabBarController class]])
+//    {
+//        rootViewController = ((UITabBarController *)rootViewController).selectedViewController;
+//    }
+//    [rootViewController presentViewController:alertController animated:YES completion:nil];
+    
   }
 }
 
