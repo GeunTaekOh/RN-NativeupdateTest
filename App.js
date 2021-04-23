4f7532aa145fe8d2f1f33e2ef602c872ca594d54
GeunTaekOh <rmsxor94@naver.com>
2021-04-23 09:26:47 +0900

    [edit ui]

--- App.js
+++ App.js
@@ -65,25 +65,16 @@ const App: () => Node = () => {
       <ScrollView
         contentInsetAdjustmentBehavior="automatic"
         style={backgroundStyle}>
-        <Header />
         <View
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
+          <Section title="iOS Native">
+            Add native app update logic
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
+          <Section title="React Native">
+          Calling the update logic of the native region
           </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
